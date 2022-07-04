58f2cbc692c5b451d07774ce799ce4474e5d09fc
Nasiru Iyidemilade <ladenas202@gmail.com>
2022-07-04 07:39:49 +0100

    fixes

--- hhf/src/views/navbar.jsx
+++ hhf/src/views/navbar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./navbar.css";
 import { Link } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 export const Navbar = () => {
   return (
@@ -19,12 +20,35 @@ export const Navbar = () => {
         <Link to="/aboutus" className="Nlink link-pos2">
           <a>About Us</a>
         </Link>
-        <Link to="/" className="Nlink link-pos2">
+        <Link to="/product" className="Nlink link-pos2">
           <a>Products</a>
         </Link>
-        <Link to="/" className="Nlink link-pos2">
+        <Link to="/contact" className="Nlink link-pos2">
           <a>Contact </a>
         </Link>
+        <div className="dropdown">
+          <Link to="#" className=" dropbtn hamburger">
+            <a>
+              <GiHamburgerMenu />
+            </a>
+          </Link>
+          <div className="dropdown-content drop">
+            <Link to="/" className="Nlink">
+              <a>Home</a>
+            </Link>
+            <Link to="/aboutus" className="Nlink">
+              <a>About Us</a>
+            </Link>
+            <Link to="/product" className="Nlink">
+              <a>Products</a>
+            </Link>
+            <Link to="/contact" className="Nlink">
+              <a>Contact</a>
+            </Link>
+          
+
+          </div>
+        </div>
       </div>
     </div>
   );
--- hhf/src/views/product.jsx
+++ hhf/src/views/product.jsx
@@ -3,9 +3,21 @@ import "../products.css"
 import { Link } from 'react-router-dom';
 import MultipleItems from './Slider';
 import Slider2 from './Slider2';
+import  { Component } from "react";
+import Slider from "react-slick";
+
 
 
 const Product = () => {
+  var settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    pauseOnHover: true
+  };
   return (
    
     <div className='page_holder'>
@@ -14,7 +26,31 @@ const Product = () => {
           Our Products
         </h1>
         <div>
-   <MultipleItems className="slider1"/>
+   {/* <MultipleItems className="slider1"/> */}
+   <div className="slider1">
+          <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />  
+          <h1 className='slider_head slider_head_center'>Stock item 1</h1>
+          <Slider {...settings}>
+            <div className="img_position">
+            <img src="/images/chicken2.jpg" className="slider-img"  alt="" />
+            </div>
+            <div className="img_position">
+            <img src="/images/chicken2.jpg" className="slider-img"   alt="" />
+            </div>
+            <div className="img_position">
+            <img src="/images/chicken2.jpg" className="slider-img"  alt="" />
+            </div>
+            <div className="img_position">
+            <img src="/images/chicken2.jpg" className="slider-img"   alt="" />
+            </div>
+            <div className="img_position">
+            <img src="/images/chicken2.jpg" className="slider-img"   alt="" />
+            </div>
+            <div className="img_position">
+            <img src="/images/chicken2.jpg" className="slider-img"   alt="" />
+            </div>
+          </Slider>
+        </div>
    <Slider2 className="slider2"/>
    <div className='slider-info'>
    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -24,7 +60,31 @@ const Product = () => {
       </div>
       </div>
       <div>
-      <MultipleItems className="slider1"/>
+      {/* <MultipleItems className="slider1"/> */}
+      <div className="slider1">
+          <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />  
+          <h1 className='slider_head_center'>Stock item 2</h1>
+          <Slider {...settings}>
+            <div className="img_position">
+            <img src="/images/chicken2.jpg" className="slider-img"  alt="" />
+            </div>
+            <div className="img_position">
+            <img src="/images/chicken2.jpg" className="slider-img"   alt="" />
+            </div>
+            <div className="img_position">
+            <img src="/images/chicken2.jpg" className="slider-img"  alt="" />
+            </div>
+            <div className="img_position">
+            <img src="/images/chicken2.jpg" className="slider-img"   alt="" />
+            </div>
+            <div className="img_position">
+            <img src="/images/chicken2.jpg" className="slider-img"   alt="" />
+            </div>
+            <div className="img_position">
+            <img src="/images/chicken2.jpg" className="slider-img"   alt="" />
+            </div>
+          </Slider>
+        </div>
    <Slider2 className="slider2"/>
    <div className='slider-info'>
    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -34,7 +94,31 @@ const Product = () => {
       </div>
       </div>
       <div>
-      <MultipleItems className="slider1"/>
+      {/* <MultipleItems className="slider1"/> */}
+      <div className="slider1">
+          <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />  
+          <h1 className='slider_head_center'>Stock item 3</h1>
+          <Slider {...settings}>
+            <div className="img_position">
+            <img src="/images/chicken2.jpg" className="slider-img"  alt="" />
+            </div>
+            <div className="img_position">
+            <img src="/images/chicken2.jpg" className="slider-img"   alt="" />
+            </div>
+            <div className="img_position">
+            <img src="/images/chicken2.jpg" className="slider-img"  alt="" />
+            </div>
+            <div className="img_position">
+            <img src="/images/chicken2.jpg" className="slider-img"   alt="" />
+            </div>
+            <div className="img_position">
+            <img src="/images/chicken2.jpg" className="slider-img"   alt="" />
+            </div>
+            <div className="img_position">
+            <img src="/images/chicken2.jpg" className="slider-img"   alt="" />
+            </div>
+          </Slider>
+        </div>
    <Slider2 />
    <div className='slider-info'>
    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
